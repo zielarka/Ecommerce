270e25648a4822f3314210f0570608302b10950a
zielarka <a.zielinskI@optiguard.pl>
2024-08-20 08:49:31 +0200

    add basket microservice

---
+++ Services/Basket/Basket.Core/Entities/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.Core.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        { 
+        
+        }
+        public ShoppingCart(string userName)
+        { 
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCashe;
+
+        public BasketRepository(IDistributedCache redisCashe)
+        {
+            _redisCashe = redisCashe;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCashe.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _redisCashe.SetStringAsync(shoppingCart.UserName, JsonConvert.SerializeObject(shoppingCart));
+            return await GetBasket(shoppingCart.UserName);
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCashe.RemoveAsync(userName);
+        }
+    }
+}
